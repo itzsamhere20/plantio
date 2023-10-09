82ab6db138e263e70262f4721ad24bb69e897f90
ehtisham-altaf <ehtisham.altaf@aiondigital.com>
2023-10-10 00:20:35 +0500

    added basic db schema and initials app

---
+++ api-development/index.js
@@ -0,0 +1,35 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const connectToMongo = require("./db");
+const app = express();
+connectToMongo();
+const appPort = process.env.PORT || 5000;
+
+
+const server = app.listen(appPort, () => {
+    console.log(`backend server is up on ${appPort}`)
+})
+
+//unexpected error handling
+process.on("uncaughtException", (err) => {
+  console.log(`Logged Error from index js: ${err.stack}`);
+  server.close(() => process.exit(1));
+})
+
+
+
+
+//with use of this our appliction will be abel to accept json inputs
+app.use(express.json({limit : 52428800})); //this is 50mb in bytes
+
+app.get('/' , async (req, res) => {   
+    res.send("HELLO WORLD")
+  })
+
+
+
+
+  
+
+
